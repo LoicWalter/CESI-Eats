aa7c01b27f464e0b0f314c07b65ff9fccb4fd7fb
Llianae <laureline.zittel@gmail.com>
2024-06-07 16:19:29 +0200

    edit tailwind config

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from 'tailwindcss';
 import sharedConfig from '@repo/tailwind-config';
 
 const config: Pick<Config, 'content' | 'presets'> = {
-  content: ['./src/app/**/*.{js,jsx,ts,tsx,css}'],
+  content: ['./src/app/**/*.tsx'],
   presets: [sharedConfig],
 };
 
