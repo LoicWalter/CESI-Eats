caecc35b0e59335982f6c626c64731c0c64432e5
Laureline Zittel <laureline.zittel@gmail.com>
2024-06-15 10:45:06 +0200

    Add responsive payement page for customer

--- apps/client/src/app/paiement/page.tsx
+++ apps/client/src/app/paiement/page.tsx
@@ -1,29 +1,28 @@
+'use client';
+
 import React from 'react';
+import BgImage from '../../assets/repas-de-famille.jpg';
 import {
-  Container,
   Typography,
-  Box,
-  Grid,
   Paper,
   Button,
-  MenuItem,
-  Select,
   FormControl,
-  InputLabel,
   IconButton,
   Modal,
   Divider,
+  Container,
 } from '@mui/material';
 import BuffaloGrill from '../../assets/BuffaloGrill.jpg';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import { PaymentForm } from '@repo/ui';
+import { PaymentForm, StyledTextField } from '@repo/ui';
 import { Close, CreditCard, Edit } from '@mui/icons-material';
 
 interface CommandeProps {
   id: string;
   restaurant: string;
+  picture: StaticImageData;
   address: string;
   deliveryAddress: string;
   card: {
@@ -42,6 +41,7 @@ interface CommandeProps {
 const commande: CommandeProps = {
   id: '1',
   restaurant: 'Buffalo Grill Fegersheim',
+  picture: BuffaloGrill,
   address: 'Rue de Lyon, N83, 67640 Fegersheim',
   deliveryAddress: '2 allée des Foulons, Lingolsheim 67380',
   card: {
@@ -70,7 +70,7 @@ const card = (
   }
   cardNumber = cardNumber.replace(/^.{14}/g, '**** **** ****').trim();
   return (
-    <div className="ui-w-full ui-flex ui-flex-row ui-gap-6">
+    <div className="w-full flex flex-row gap-6">
       <div>{cardNumber}</div>
       <div>{cardDate}</div>
     </div>
@@ -82,7 +82,6 @@ export default function OrderDetails() {
     cardNumber: Yup.string()
       // .matches(/^[0-9]{16}$/, "Invalid card number")
       .required('Card number is required'),
-    // expiryDate: yup.string().required("Expiry date is required"),
     expiryDate: Yup.string()
       .required('Expiry date is required')
       .test('valid-month', 'Invalid month', function (value) {
@@ -120,217 +119,217 @@ export default function OrderDetails() {
   return (
     <Formik
       initialValues={{
-        cardName: commande.card.cardName,
-        cardNumber: commande.card.cardNumber,
-        expiryDate: commande.card.expiryDate,
-        cvv: commande.card.cvv,
+        id: commande.id,
+        restaurant: commande.restaurant,
+        picture: commande.picture,
+        address: commande.address,
+        deliveryAddress: commande.deliveryAddress,
+        card: commande.card,
+        items: commande.items,
+        serviceFee: commande.serviceFee,
       }}
-      onSubmit={(values: Record<string, any>) => console.log(values)}
+      onSubmit={(values: CommandeProps) => console.log(values)}
     >
       {(formik) => (
         <form
           onSubmit={formik.handleSubmit}
-          className="ui-flex w-full"
+          className="flex w-full"
         >
-          <Container
-            maxWidth="md"
-            className="my-8"
-          >
-            <Box className="flex flex-col items-start mb-8">
-              <Typography
-                variant="h4"
-                component="h1"
-                className="font-bold mb-4"
-              >
-                Détails de la livraison
-              </Typography>
-              <Box className="flex w-full mb-8">
-                <Image
-                  src={BuffaloGrill}
-                  alt="Buffalo Grill"
-                  className="w-1/3 rounded"
-                />
-                <Box className="ml-8">
-                  <Typography
-                    variant="h6"
-                    component="h2"
-                    className="font-bold"
-                  >
-                    Buffalo Grill Fegersheim
-                  </Typography>
-                  <Typography variant="body1">
-                    Rue de Lyon, N83,
-                    <br />
-                    67640 Fegersheim
-                  </Typography>
-                </Box>
-              </Box>
-              <Grid
-                container
-                spacing={4}
-              >
-                <Grid
-                  item
-                  xs={12}
-                  md={6}
+          <div className="md:h-screen h-full md:overflow-hidden w-full flex flex-col justify-center items-center">
+            <Image
+              src={BgImage}
+              alt="Repas de famille"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
+            <Container
+              maxWidth="md"
+              className="h-full flex item-center justify-center bg-white z-20"
+            >
+              <div className="flex flex-col w-full px-6 mt-2 md:p-12 item-center md:justify-between">
+                <Typography
+                  variant="h3"
+                  className="font-bold mb-2 md:mb-6"
                 >
-                  <FormControl fullWidth>
-                    <InputLabel id="address-label">Adresse de livraison</InputLabel>
-                    <Select
-                      labelId="address-label"
-                      id="address"
-                      value="2 allée des Foulons, Lingolsheim 67380"
-                    >
-                      <MenuItem value="2 allée des Foulons, Lingolsheim 67380">
-                        2 allée des Foulons, Lingolsheim 67380
-                      </MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  md={6}
-                >
-                  <FormControl fullWidth>
-                    <div className="ui-flex ui-flex-col ui-gap-2">
-                      <Typography variant="h6">Carte banquaire</Typography>
-                      <div className="ui-flex ui-flex-row ui-gap-2 ui-items-center">
-                        <div className="ui-justify-between ui-flex ui-flex-row ui-w-full">
-                          <div className="ui-flex ui-flex-row ui-gap-2">
-                            <CreditCard className="ui-opacity-55" />
+                  <div className="md:text-4xl sm:text-3xl text-2xl">Détails de la livraison</div>
+                </Typography>
+                <div className="flex w-full mb-4 md:flex-row flex-col">
+                  <Image
+                    src={commande.picture}
+                    alt="Buffalo Grill"
+                    className="md:w-1/2 w-full rounded-xl aspect-video justify-center object-cover object-center align-center "
+                  />
+                  <div className="md:ml-8 flex flex-col gap-3 w-full md:mt-0 mt-2">
+                    <div className="flex flex-col gap-1">
+                      <Typography
+                        variant="h5"
+                        className="font-bold"
+                      >
+                        <div className="md:text-2xl sm:text-xl text-lg">{commande.restaurant}</div>
+                      </Typography>
+                      <Typography variant="body1">
+                        <div className="md:text-base text-sm">{commande.address}</div>
+                      </Typography>
+                    </div>
+                    <div className="flex flex-col gap-2 w-full">
+                      <div className="flex flex-col gap-2 w-full">
+                        <Typography
+                          variant="h6"
+                          className="font-bold"
+                        >
+                          <div className="md:text-xl text-base">Adresse de livraison</div>
+                        </Typography>
+                        <StyledTextField
+                          fullWidth
+                          variant="standard"
+                          name="deliveryAddress"
+                          value={commande.deliveryAddress}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          error={Boolean(
+                            formik.errors.deliveryAddress && formik.touched.deliveryAddress,
+                          )}
+                          helperText={
+                            formik.errors.deliveryAddress && formik.touched.deliveryAddress
+                              ? formik.errors.deliveryAddress
+                              : ''
+                          }
+                          InputProps={{
+                            className: `${formik.errors.deliveryAddress && formik.touched.deliveryAddress ? 'bg-red-100' : ''}`,
+                          }}
+                        />
+                      </div>
+                      <div>
+                        <FormControl fullWidth>
+                          <div className="flex flex-col gap-2">
+                            <Typography
+                              variant="h6"
+                              className="font-bold"
+                            >
+                              <div className="md:text-xl text-base">Carte banquaire</div>
+                            </Typography>
+                            <div className="flex flex-row gap-2 items-center">
+                              <div className="justify-between flex flex-row w-full">
+                                <div className="flex flex-row gap-2">
+                                  <CreditCard className="opacity-55" />
 
-                            <Typography variant="body1">
-                              {card(
-                                formik.values.cardName,
-                                formik.values.cardNumber,
-                                formik.values.expiryDate,
-                                formik.values.cvv,
-                              )}
+                                  <Typography variant="body1">
+                                    {card(
+                                      formik.values.card.cardName,
+                                      formik.values.card.cardNumber,
+                                      formik.values.card.expiryDate,
+                                      formik.values.card.cvv,
+                                    )}
+                                  </Typography>
+                                </div>
+                                <IconButton
+                                  className="p-0"
+                                  onClick={handleClickOpen}
+                                >
+                                  <Edit />
+                                </IconButton>
+                                <Modal
+                                  open={open}
+                                  onClose={handleClose}
+                                  className="flex justify-center items-center "
+                                  title="Ajouter une carte banquaire"
+                                >
+                                  <div className="bg-gray-5 m-8 p-8 rounded-lg relative">
+                                    <IconButton
+                                      className="absolute top-4 right-4"
+                                      onClick={handleClose}
+                                    >
+                                      <Close />
+                                    </IconButton>
+                                    <PaymentForm formik={formik} />
+                                    <Button
+                                      variant="outlined"
+                                      className="bg-primary text-white hover:bg-secondary rounded-lg border-primary hover:border-secondary w-full py-2 mt-8"
+                                      onClick={handleClose}
+                                    >
+                                      Enregistrer
+                                    </Button>
+                                  </div>
+                                </Modal>
+                              </div>
+                            </div>
+                            <Divider />
+                          </div>
+                        </FormControl>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full">
+                  <Paper className="p-4">
+                    <div className="flex flex-col gap-4">
+                      <div className="flex flex-col gap-2">
+                        {Object.entries(commande.items).map(([key, value]) => (
+                          <div
+                            className="w-full flex flex-row justify-between"
+                            key={key}
+                          >
+                            <Typography variant="body1">{value.name}</Typography>
+                            <Typography
+                              variant="body1"
+                              className="text-right items-center justify-center flex"
+                            >
+                              {value.price}€
                             </Typography>
                           </div>
-                          <IconButton
-                            className="ui-p-0"
-                            onClick={handleClickOpen}
+                        ))}
+                        <div className="w-full flex flex-row justify-between">
+                          <Typography variant="body1">Frais de livraison</Typography>
+                          <Typography
+                            variant="body1"
+                            className="text-right items-center justify-center flex"
                           >
-                            <Edit />
-                          </IconButton>
-                          <Modal
-                            open={open}
-                            onClose={handleClose}
-                            className="ui-flex ui-justify-center ui-items-center "
-                            title="Ajouter une carte banquaire"
-                          >
-                            <div className="ui-bg-gray-5 ui-m-8 ui-p-8 ui-rounded-lg ui-relative">
-                              <IconButton
-                                className="ui-absolute ui-top-4 ui-right-4"
-                                onClick={handleClose}
-                              >
-                                <Close />
-                              </IconButton>
-                              <PaymentForm formik={formik} />
-                              <Button
-                                variant="outlined"
-                                className="ui-bg-primary ui-text-white hover:ui-bg-secondary ui-rounded-lg ui-border-primary hover:ui-border-secondary ui-w-full ui-py-2 ui-mt-8"
-                                onClick={handleClose}
-                              >
-                                Enregistrer
-                              </Button>
-                            </div>
-                          </Modal>
+                            {commande.serviceFee}€
+                          </Typography>
                         </div>
                       </div>
-                      <Divider />
+                      <div className="w-full flex flex-row justify-between">
+                        <Typography
+                          variant="body1"
+                          className="font-bold"
+                        >
+                          Total :
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          className="text-right font-bold items-center justify-center flex"
+                        >
+                          {commande.items.reduce(
+                            (acc, item) => acc + item.price,
+                            commande.serviceFee,
+                          )}
+                          €
+                        </Typography>
+                      </div>
                     </div>
-                  </FormControl>
-                </Grid>
-              </Grid>
-            </Box>
-            <Box className="w-full">
-              <Paper className="p-4">
-                <Grid
-                  container
-                  spacing={2}
-                >
-                  <Grid
-                    item
-                    xs={8}
-                  >
-                    <Typography variant="body1">Super Famous Burger Bacon</Typography>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={4}
-                  >
-                    <Typography
-                      variant="body1"
-                      className="text-right"
+                  </Paper>
+                </div>
+                <div className="flex justify-between mt-6 w-full">
+                  <div className="flex flex-row items-center gap-2 justify-end w-full pb-4 md:pb-0">
+                    <Button
+                      variant="outlined"
+                      className="text-primary hover:text-secondary md:w-1/3 w-full border-primary rounded-lg hover:border-secondary "
                     >
-                      11€50
-                    </Typography>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={8}
-                  >
-                    <Typography variant="body1">Frais de service</Typography>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={4}
-                  >
-                    <Typography
-                      variant="body1"
-                      className="text-right"
+                      Annuler
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      type="submit"
+                      className="bg-primary text-white hover:bg-secondary rounded-lg border-primary hover:border-secondary md:w-1/3 w-full"
                     >
-                      1€50
-                    </Typography>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={8}
-                    className="mt-4"
-                  >
-                    <Typography
-                      variant="body1"
-                      className="font-bold"
-                    >
-                      Total :
-                    </Typography>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={4}
-                    className="mt-4"
-                  >
-                    <Typography
-                      variant="body1"
-                      className="text-right font-bold"
-                    >
-                      13€00
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Paper>
-            </Box>
-            <Box className="flex justify-between mt-8 w-full">
-              <div className="ui-flex ui-flex-row ui-items-center ui-gap-2 ui-justify-end ui-w-full">
-                <Button
-                  variant="outlined"
-                  className="ui-text-primary hover:ui-text-secondary md:ui-w-1/5 ui-w-full ui-border-primary ui-rounded-lg hover:ui-border-secondary "
-                >
-                  Annuler
-                </Button>
-                <Button
-                  variant="outlined"
-                  type="submit"
-                  className="ui-bg-primary ui-text-white hover:ui-bg-secondary ui-rounded-lg ui-border-primary hover:ui-border-secondary md:ui-w-1/5 ui-w-full"
-                >
-                  Commander
-                </Button>
+                      Commander
+                    </Button>
+                  </div>
+                </div>
               </div>
-            </Box>
-          </Container>
+            </Container>
+          </div>
         </form>
       )}
     </Formik>
