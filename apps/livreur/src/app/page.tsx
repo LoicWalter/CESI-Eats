9223427c5d2039684a9b60c198a3916c8eca0149
loic <loicwalter88@gmail.com>
2024-06-21 01:08:45 +0200

    fix profile

--- apps/livreur/src/app/page.tsx
+++ apps/livreur/src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { AllDeliveries } from '@repo/ui';
+import { AllDeliveries, DeliveriesProvider } from '@repo/ui';
 
 export default function Page(): JSX.Element {
   return (
-    <div className="flex flex-col items-center justify-between h-screen">
-      <AllDeliveries />
-    </div>
+    <DeliveriesProvider>
+      <div className="flex flex-col items-center justify-between h-screen">
+        <AllDeliveries />
+      </div>
+    </DeliveriesProvider>
   );
 }
