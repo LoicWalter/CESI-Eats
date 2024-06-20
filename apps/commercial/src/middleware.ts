9a5e44b2c4fdf4ebffda3dd334a7c46cadf5eb64
Laureline Zittel <laureline.zittel@gmail.com>
2024-06-20 19:16:01 +0200

    dashboard for commercial

--- apps/commercial/src/middleware.ts
+++ apps/commercial/src/middleware.ts
@@ -1,32 +1,21 @@
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest): Response | undefined {
-  const role = 'ADMIN';
   const app = '/commercial';
-  const currentUser = request.cookies.get('User')?.value;
   const authCookie = request.cookies.get('Authentication')?.value;
-  const isLogged = Boolean(currentUser && authCookie);
+  const isLogged = Boolean(authCookie);
   const currentPathName = request.nextUrl.pathname;
-
-  const userRole = JSON.parse(currentUser || '{}')?.roles;
 
   const authRoutes = ['/auth/login', '/auth/signup'];
   const routesNeedAuth = ['/profil', '/paiement'];
-  const routesNeedRole = ['/profil', '/paiement', '/dashboard'];
 
-  const isAuthRoute = authRoutes.some((route) => currentPathName.startsWith(route));
+  const isAuthRoute = authRoutes.some((route) => currentPathName.includes(route));
 
   if (isLogged && isAuthRoute) {
     return Response.redirect(new URL(app, request.url));
   }
 
-  if (routesNeedRole.some((route) => currentPathName.startsWith(route))) {
-    if (!userRole || !userRole.includes(role)) {
-      return Response.redirect(new URL(`${app}/auth/login`, request.url));
-    }
-  }
-
-  if (!isLogged && routesNeedAuth.some((route) => currentPathName.startsWith(route))) {
+  if (!isLogged && routesNeedAuth.some((route) => currentPathName.includes(route))) {
     return Response.redirect(new URL(`${app}/auth/login`, request.url));
   }
 }
